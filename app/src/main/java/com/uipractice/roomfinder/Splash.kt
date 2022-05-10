98ce6c4b67ff3c484d4dfdf7385060af8e5c5d25
Brijesh <brijeshbarasiya44@gmail.com>
2022-05-10 10:58:34 +0530

    Splash Screen

---
+++ app/src/main/java/com/uipractice/roomfinder/Splash.kt
@@ -0,0 +1,31 @@
+package com.uipractice.roomfinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.uipractice.roomfinder.authentication.LoginScreen
+import com.uipractice.roomfinder.databinding.SplashScreenBinding
+
+class Splash : AppCompatActivity() {
+
+    // Variable
+    private lateinit var binding: SplashScreenBinding
+
+    // Override Method
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, LoginScreen::class.java).apply {
+                startActivity(this)
+            }
+            finish()
+        }, 2000)
+    }
+
+}
